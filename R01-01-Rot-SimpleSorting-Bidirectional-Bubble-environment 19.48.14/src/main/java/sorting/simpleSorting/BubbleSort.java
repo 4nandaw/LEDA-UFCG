3d234e41a597c84ee17ed3a83eff77720ae8f245
Ananda Vilar Vidal <ananda.vilar.vidal@ccc.ufcg.edu.br>
2023-08-15 21:39:18 -0300

    BubbleSort

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment 19.48.14/src/main/java/sorting/simpleSorting/BubbleSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment 19.48.14/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -12,16 +12,32 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		boolean swapped = true;
+		if (validation(array, leftIndex, rightIndex)) {
+			boolean swapped = true;
 
-        while (swapped) {
-            swapped = false;
-            for (int i = leftIndex; i < rightIndex; i++) {
-                if (array[i].compareTo(array[i + 1]) > 0) {
-                    Util.swap(array, i, i + 1);
-                    swapped = true;
-                }
-            }
-        }
+        	while (swapped) {
+            	swapped = false;
+            	for (int i = leftIndex; i < rightIndex; i++) {
+                	if (array[i].compareTo(array[i + 1]) > 0) {
+                    	Util.swap(array, i, i + 1);
+                    	swapped = true;
+                	}
+            	}
+			}
+		}
+	}
+
+	private boolean validation(T[] array, int leftIndex, int rightIndex) {
+		boolean isValid = true;
+
+		if (array == null || array.length == 0) {
+			isValid = false;
+		} else if ((leftIndex >= rightIndex) || (leftIndex < 0) || (rightIndex <= 0)) {
+			isValid = false;
+		} else if ((rightIndex > array.length - 1) || leftIndex >= array.length) {
+			isValid = false;
+		}
+
+		return isValid;
 	}
 }
