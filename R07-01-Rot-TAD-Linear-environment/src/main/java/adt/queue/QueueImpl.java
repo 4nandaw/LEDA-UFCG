770bfa5cb86927418ab55dd6a4d218efdef6aeb7
4nandaw <ananda.vilar.vidal@ccc.ufcg.edu.br>
2023-09-13 08:51:05 -0300

    QueueImpl

--- R07-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueImpl.java
+++ R07-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueImpl.java
@@ -22,12 +22,12 @@ public class QueueImpl<T> implements Queue<T> {
 
 	@Override
 	public boolean isEmpty() {
-		return this.tail == - 1;
+		return (this.tail == - 1);
 	}
 
 	@Override
 	public boolean isFull() {
-		return this.tail == this.array.length - 1;
+		return (this.tail == this.array.length - 1);
 	}
 
 	private void shiftLeft() {
